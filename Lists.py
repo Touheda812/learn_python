470001d6324afbdea3a61e8c779d049737a6b60a
Touheda Khanom <TouhedaKhanom14@gmail.com>
2022-10-04 01:00:45 -0400

    Python Strings

---
+++ Lists.py
@@ -0,0 +1,14 @@
+'''
+list are mutable
+
+'''
+list_1 = [1, 2, 3, 4, 5]
+print(list_1)
+
+# list indexing and slicing
+print(list_1[0])  # index
+print(list_1[-3:])  # slice
+
+# list concatenation
+print(list_1 + [6,7,8,9,10])
+
