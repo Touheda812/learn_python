991f0bac98b104d576d632de5c12a88bb146f122
Touheda Khanom <TouhedaKhanom14@gmail.com>
2022-10-03 22:02:10 -0400

    Python Strings

---
+++ Strings.py
@@ -0,0 +1,11 @@
+'''
+backslash(\) used to escape quotes
+
+'''
+# print('doesn't') SyntaxError: unterminated string literal
+print('doesn\'t')
+print("doesn't")
+print('"Yes,"they said.') #sigle quote
+print("\"Yes,\" they said.")
+s = 'First line.\nSecond line.'
+print(s)
