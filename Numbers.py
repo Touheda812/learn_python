0feced0aec3154608b7a27ef55859b951913dab5
Touheda Khanom <TouhedaKhanom14@gmail.com>
2022-10-03 20:56:22 -0400

    Initial commit

---
+++ Numbers.py
@@ -0,0 +1,19 @@
+'''
+Operators: + , - , * , /
+Data Types: int, float
+Division (/) which returns float but Floor Division (//) returns integer discarding any fractional result
+Multiplication (*)
+squared (**)
+
+'''
+
+# Floor division(//) vs division(/)
+print(8 / 3)
+print(8 // 3)
+
+# Squared(**) vs multiplication(*)
+print(2 * 3)
+print(2 ** 3)
+
+
+
