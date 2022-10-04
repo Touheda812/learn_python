a83539ced830e6dd8b0abb23e043188896b3eed4
Touheda Khanom <TouhedaKhanom14@gmail.com>
2022-10-03 23:06:25 -0400

    Python Strings

--- Strings.py
+++ Strings.py
@@ -3,6 +3,10 @@ Strings are immutable
 backslash(\) used to escape quotes
 raw strings\
 span string multiple lines
+print multiple times using *
+concatenate using +
+string indexing and slicing
+string methods: capitalize(), casefold(), center(width[,fillchar])
 '''
 # print('doesn't') SyntaxError: unterminated string literal
 print('doesn\'t')
@@ -40,20 +44,44 @@ print(txt + 'thon')
 # indexing
 print('----------String Indexing-----------')
 word = 'Touheda'
-print(word[0]) #first character
+print(word[0])  # first character
 print(word[5])
-print(word[-1]) #last character
+print(word[-1])  # last character
 print(word[-6])
-print('Length of the word is:',len(word))
+print('Length of the word is:', len(word))
 
 print('-----String Slicing------')
 print(word[0:2])
 print(word[2:7])
 
-print(word[:2]) # character from the beginning to position 2 (excluded)
-print(word[4:]) # characters from position 4 (included) to the end
+print(word[:2])  # character from the beginning to position 2 (excluded)
+print(word[4:])  # characters from position 4 (included) to the end
 
 print(word[:2] + word[2:])
 
 # create new string
 print('J' + word[1:])
+
+# String Methods
+# capitalize
+string = 'mother'
+print('capitalize string:', string.capitalize())
+
+# lower case
+string_1 = 'Father'
+print('lowercase string:', string_1.casefold())
+
+# center
+string_2 = 'This is the book'
+print(string_2.center(50))
+
+# Title()
+print('hello world'.title())
+print("they're bill's friends from the UK".title())
+
+# split()
+print('1,2,3'.split())
+print('1<>2<>3'.split('<>'))
+
+print(string_2.swapcase())
+# output("tHIS IS THE BOOK")
